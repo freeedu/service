eee1519635037b3209a1fb27e9d8ea15a100b376
mason <dongxu.m@gmail.com>
2013-11-11 22:41:56 +0800

    Add a genaral entity model for more flex able function #14

---
+++ FEOP/FEOPOAuth/FEOPOAuthAccount/src/main/java/org/personal/mason/feop/oauth/contact/domain/model/ContactInfoType.java
@@ -0,0 +1,43 @@
+package org.personal.mason.feop.oauth.contact.domain.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contact_info_type")
+@NamedQuery(name = "ContactInfoType.findAll", query = "SELECT t FROM ContactInfoType t")
+public class ContactInfoType extends FOEPPersistable<Long> {
+
+private static final long serialVersionUID = -5212794572090869477L;
+
+@Column(name = "name", nullable = false, length = 40)
+private String name;
+@Column(name = "description", length = 255)
+private String description;
+
+// bi-directional many-to-one association to ContactPhone
+@OneToMany(mappedBy = "infoType")
+private List<ContactInfoCommon> infoCommon;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+}
--- FEOP/FEOPOAuth/FEOPOAuthAccount/src/main/java/org/personal/mason/feop/oauth/contact/mvc/rest/ContactQueryApi.java
+++ FEOP/FEOPOAuth/FEOPOAuthAccount/src/main/java/org/personal/mason/feop/oauth/contact/mvc/rest/ContactQueryApi.java
@@ -1,10 +1,28 @@
 package org.personal.mason.feop.oauth.contact.mvc.rest;
 
+import java.util.Date;
+import java.util.List;
+
+import org.personal.mason.feop.oauth.contact.mvc.model.Contact;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("contact/q")
 public class ContactQueryApi {
+
+public List<Contact> findContactByRemindDate(Long accountId, Date date) {
+	throw new RuntimeException("Not Implemented Exception");
+}
+
+public List<Contact> findUpdatedContact(Long accountId) {
+	throw new RuntimeException("Not Implemented Exception");
+}
+
+public Contact findContact(Long contactId) {
+	throw new RuntimeException("Not Implemented Exception");
+}
+
+
 
 }
