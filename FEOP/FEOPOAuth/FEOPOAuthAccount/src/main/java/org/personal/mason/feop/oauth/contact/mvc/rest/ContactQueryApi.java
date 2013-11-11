a0d0b3029b185f58d9c1cd4daee444a721462850
mason <dongxu.m@gmail.com>
2013-11-11 23:04:57 +0800

    Define the restful API for contacts purpose #13

--- FEOP/FEOPOAuth/FEOPOAuthAccount/src/main/java/org/personal/mason/feop/oauth/contact/mvc/rest/ContactQueryApi.java
+++ FEOP/FEOPOAuth/FEOPOAuthAccount/src/main/java/org/personal/mason/feop/oauth/contact/mvc/rest/ContactQueryApi.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.personal.mason.feop.oauth.contact.mvc.model.Contact;
+import org.personal.mason.feop.oauth.contact.mvc.model.Record;
+import org.personal.mason.feop.oauth.contact.mvc.model.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,6 +25,20 @@ public Contact findContact(Long contactId) {
 	throw new RuntimeException("Not Implemented Exception");
 }
 
+public List<Contact> findContactWithRecord(Long accountId, Record record) {
+	throw new RuntimeException("Not Implemented Exception");
+}
 
+public List<Contact> findContactWithResource(Long accountId, Resource resource) {
+	throw new RuntimeException("Not Implemented Exception");
+}
+
+public List<Contact> findContactWithString(Long accountId, String query) {
+	throw new RuntimeException("Not Implemented Exception");
+}
+
+public List<Contact> findContactsKnown(Long accoutId) {
+	throw new RuntimeException("Not Implemented Exception");
+}
 
 }
